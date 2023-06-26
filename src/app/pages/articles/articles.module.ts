d58383d5a9aa2f931e3dabea7d13c090690b4143
jawaddev12 <jawad.ahmad@arhpez.com>
2023-06-26 16:00:34 +0500

    fix errors

--- src/app/pages/articles/articles.module.ts
+++ src/app/pages/articles/articles.module.ts
@@ -7,8 +7,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AgmCoreModule } from '@agm/core';  
 import { SharedModule } from '../../shared/shared.module';
-import { ArticlesComponent } from './Articles.component';
 import { HomeComponent } from '../home/home.component';
+import { ArticlesComponent } from './articles.component';
 export const routes: Routes = [
     { path: '', component: ArticlesComponent, pathMatch: 'full' },
     // { path: ':id', component: PropertyComponent },
